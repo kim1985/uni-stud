3d6d42c24feb2e822d0cab250c5751a2580564b9
jonny.daidone <jonny.daidone@lynxspa.com>
2025-07-08 17:32:41 +0200

    init project

---
+++ src/main/java/com/uni/unistud/UniStudApplication.java
@@ -0,0 +1,13 @@
+package com.uni.unistud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniStudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniStudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni/unistud/UniStudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.unistud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniStudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
