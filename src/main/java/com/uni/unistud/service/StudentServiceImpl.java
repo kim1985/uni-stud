69ed9253ab00f7b111ad3c853a328e94beb88137
jonny.daidone <jonny.daidone@lynxspa.com>
2025-07-09 17:22:43 +0200

    create exception handler

--- src/main/java/com/uni/unistud/service/StudentServiceImpl.java
+++ src/main/java/com/uni/unistud/service/StudentServiceImpl.java
@@ -3,13 +3,12 @@ package com.uni.unistud.service;
 import com.uni.unistud.dto.StudentDTO;
 import com.uni.unistud.entity.Course;
 import com.uni.unistud.entity.Student;
+import com.uni.unistud.exception.ResourceNotFoundException;
 import com.uni.unistud.repository.CourseRepository;
 import com.uni.unistud.repository.StudentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -30,20 +29,19 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public StudentDTO getStudentById(Long studentId) {
-        Student student = studentRepository.findById(studentId).orElseThrow(RuntimeException::new);
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Student", "studentId", studentId));
         return modelMapper.map(student, StudentDTO.class);
     }
 
     @Override
     public void addCourseToStudent(Long studentId, Long courseId) {
-        Optional<Student> studentOptional = studentRepository.findById(studentId);
-        Optional<Course> courseOptional = courseRepository.findById(courseId);
-        if (studentOptional.isPresent() && courseOptional.isPresent()) {
-            Student student = studentOptional.get();
-            Course course = courseOptional.get();
-            student.addCourse(course);
-            studentRepository.save(student);
-        }
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Student", "studentId", studentId));
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course", "courseId", courseId));
+        student.addCourse(course);
+        studentRepository.save(student);
 
     }
 }
